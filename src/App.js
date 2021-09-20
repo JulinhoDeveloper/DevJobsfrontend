0d5745896b5ca51cefba12730c4fb1a272011987
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-09-20 18:03:59 -0300

    Início do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'antd/dist/antd.css';
+import Home from './pages/Home';
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '../components/defaultLayout'
+function Home() {
+    return (
+        <div>
+            <DefaultLayout>
+                <h1>Home Page</h1>
+            </DefaultLayout>
+        </div>
+    )
+}
+
+export default Home
